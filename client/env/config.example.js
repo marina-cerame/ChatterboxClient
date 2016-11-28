9cbe60eb91b9691836ca966a4ef8930c68949a35
Marina Cerame <marina.cerame@gmail.com>
2016-11-28 16:28:59 -0600

    working out the app methods

--- client/env/config.example.js
+++ client/env/config.example.js
@@ -2,7 +2,9 @@
 // request that you make so you don't have to.
 
 // Put your parse application keys here!
+
+
 $.ajaxPrefilter(function (settings, _, jqXHR) {
-  jqXHR.setRequestHeader('X-Parse-Application-Id', 'PARSE_APP_ID');
-  jqXHR.setRequestHeader('X-Parse-REST-API-Key', 'PARSE_API_KEY');
+  jqXHR.setRequestHeader('X-Parse-Application-Id', 'wPfavNOxOKTzlXiMq9r226nSo5Vl5aMFfEcvAupE');
+  jqXHR.setRequestHeader('X-Parse-REST-API-Key', 'eCGqXWtDjoZofGzf785KvSODrUrXURbL6JCQqaHj');
 });
--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,27 @@
 // YOUR CODE HERE:
+var app = {};
+
+app.init = () => {
+
+};
+
+app.send = message => {
+  $.ajax({
+    url: 'https://api.parse.com/1/classes/messages',
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: data => {
+      console.log('chatterbox: Message sent');
+    },
+    error: data => {
+      console.error('chatterbox: Failed to send message', data);
+    }
+  });
+};
+
+app.fetch = () => {
+  $.ajax({
+    type: 'GET'
+  });
+};
